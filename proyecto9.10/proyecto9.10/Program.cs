adfb7b776cb0b9e6d059d9afac9e7831e5b292f2
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-12-28 12:31:43 -0800

    version1.1

--- proyecto9.10/proyecto9.10/Program.cs
+++ proyecto9.10/proyecto9.10/Program.cs
@@ -15,9 +15,13 @@ namespace proyecto9._10
                 int[] lista1 = new int[15];
                 int[] lista2 = new int[15];
                 int vuelta = 0;
-                int numero, ValorLista1;
+                int numero;
+                int TotalLista1 = 0;
+                int TotalLista2 = 0;
+
                 Console.WriteLine("Esta es la 1ra lista, presione enter para iniciar: ");
                 Console.ReadKey();
+                
                 while (vuelta<15)
                 {
                     numero = vuelta + 1;
@@ -25,13 +29,45 @@ namespace proyecto9._10
                     lista1[vuelta] = int.Parse(Console.ReadLine());
                     vuelta++;
                 }
+                vuelta = 0;
+                
+                Console.WriteLine("Esta es la 2da lista, presione enter para iniciar: ");
+                Console.ReadKey();
+               
+                while (vuelta < 15)
+                {
+                    numero = vuelta + 1;
+                    Console.Write("Introduce el valor #" + numero + ": ");
+                    lista2[vuelta] = int.Parse(Console.ReadLine());
+                    vuelta++;
+                }
+                vuelta = 0;
 
+                while (vuelta < 15)
+                {
+                    TotalLista1 = TotalLista1 + lista1[vuelta];
+                    TotalLista2 = TotalLista2 + lista2[vuelta];
+                    vuelta++;
+                }
+
+                if (TotalLista1>TotalLista2)
+                {
+                    Console.WriteLine("Lista 1 mayor");
+                }
+                else if(TotalLista2>TotalLista1)
+                {
+                    Console.WriteLine("Lista 2 mayor");
+                }
+                else
+                {
+                    Console.WriteLine("Listas iguales");
+                }               
             }
             catch (Exception)
             {
-                Console.WriteLine("Usted ha introducido un valor invalido");
-                throw;
+                Console.WriteLine("Usted ha introducido un valor invalido");                
             }
+            Console.ReadKey();
         }
     }
 }
